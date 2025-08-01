0f8cc78b2c1395d821589016fb034d316f174660
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-01 19:16:32 +0530

    updated public

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "images.unsplash.com",
+      "plus.unsplash.com",
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
