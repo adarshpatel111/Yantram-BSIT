e9744e93ce6b57cf8bcc2c84973f17c22628e77a
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-20 23:15:43 +0530

    Update contants.ts

--- src/utilities/contants.ts
+++ src/utilities/contants.ts
@@ -1,6 +1,3 @@
 export const BASE_URL = process.env.BASE_URL as string;
 
 export const DATABASE_URL = process.env.DATABASE_URL as string;
-
-// export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID as string;
-// export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET as string;
