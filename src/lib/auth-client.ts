0efb22ade291fecb52145b24d64ee89f1aaa1adf
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-21 01:48:05 +0530

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,13 @@
+import { auth } from "./auth";
 import { BASE_URL } from "@/utilities/contants";
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
-import { auth } from "./auth";
+import { adminClient } from "better-auth/client/plugins";
+
 export const authClient = createAuthClient({
   baseURL: BASE_URL,
   plugins: [
+    adminClient(),
     inferAdditionalFields<typeof auth>({
       user: {
         phone: {
