e3f13e9b2af7b980a65464d4002effad6a878065
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-08-21 01:48:16 +0530

    Create route.tsx

---
+++ src/app/api/account-with-role/route.tsx
@@ -0,0 +1,22 @@
+import { auth } from "@/lib/auth";
+import { withRoleCheck } from "@/lib/withRoleCheck";
+import { NextRequest, NextResponse } from "next/server";
+
+async function createAccount(req: NextRequest) {
+  const session = await auth.api.getSession({
+    headers: req.headers,
+  });
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { email, password, name, role, data } = await req.json();
+  const newUser = await auth.api.createUser({
+    body: { email, password, name, role, data },
+  });
+
+  return NextResponse.json({ user: newUser });
+}
+
+export const POST = withRoleCheck(["admin"])(createAccount);
