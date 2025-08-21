ee2f29a262f1c2e4761d1ae6b08aef4d87f50c28
adarshpatelBSIT <150321409+adarshpatelBSIT@users.noreply.github.com>
2025-08-21 17:11:37 +0530

    Update page.tsx

--- src/app/(dashboard)/dashboard/purchases/page.tsx
+++ src/app/(dashboard)/dashboard/purchases/page.tsx
@@ -6,9 +6,10 @@ import { useQuery } from "@tanstack/react-query";
 import { IPurchase } from "@/types/purchases";
 import { UsersPagination } from "./pagination";
 import { DataTable } from "./data-table";
-import { columns } from "./columns";
+import PageLimitSelector from "@/components/PageLimitSelector";
+import { authClient } from "@/lib/auth-client";
+import { getColumns } from "./columns";
 
-// API fetch function
 const fetchPurchases = async ({
   page,
   limit,
@@ -21,8 +22,11 @@ const fetchPurchases = async ({
 };
 
 const PurchasesPage: React.FC = () => {
+  const { data: session } = authClient.useSession();
+  const role = session?.user?.role;
+
   const [page, setPage] = useState(1);
-  const limit = 5; // rows per page
+  const [limit, setLimit] = useState(5);
 
   const { data, isPending } = useQuery({
     queryKey: ["purchases", page, limit],
@@ -31,23 +35,32 @@ const PurchasesPage: React.FC = () => {
 
   if (isPending) return <div className="p-4">Loading purchases...</div>;
 
+  if (role !== "admin" && role !== "manager") {
+    return (
+      <div className="flex items-center justify-center text-3xl font-semibold">
+        Unauthorized
+      </div>
+    );
+  }
+
   return (
     <div className="py-5 space-y-4">
       <h2 className="text-xl font-semibold">Purchases</h2>
 
-      {/* Table */}
       <DataTable
-        columns={columns}
+        columns={getColumns(data?.data || [])}
         data={data?.data || []}
         key={`purchases-table-${page}`}
       />
+      <div className="w-full flex items-center justify-between">
+        <PageLimitSelector limit={limit} setLimit={setLimit} />
 
-      {/* Pagination */}
-      <UsersPagination
-        currentPage={page}
-        totalPages={data?.totalPages || 1}
-        onPageChange={(newPage) => setPage(newPage)}
-      />
+        <UsersPagination
+          currentPage={page}
+          totalPages={data?.totalPages || 1}
+          onPageChange={(newPage) => setPage(newPage)}
+        />
+      </div>
     </div>
   );
 };
