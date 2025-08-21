12cb2624ba7e9d4fdc9901990915e84caf1fa6e2
adarshpatelBSIT <150321409+adarshpatelBSIT@users.noreply.github.com>
2025-08-21 17:11:53 +0530

    Update dash-navbar.tsx

--- src/components/dashboard/dash-navbar.tsx
+++ src/components/dashboard/dash-navbar.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
 import { DynamicBreadcrumb } from "@/components/dashboard/dynamic-breadcrumb";
+import { authClient } from "@/lib/auth-client";
 
 const DashNavbar = () => {
+  const { data: session } = authClient.useSession();
+  const branch = session?.user?.branch;
   return (
     <header className="fixed top-0 right-0 left-0 z-40 flex h-14 shrink-0 justify-between items-center gap-2 border bg-background transition-[width,height] ease-linear md:left-64 md:w-[calc(100%-16rem)] group-has-data-[collapsible=icon]/sidebar-wrapper:md:left-12 group-has-data-[collapsible=icon]/sidebar-wrapper:md:w-[calc(100%-2rem)]">
       <div className="flex items-center gap-2 px-4">
@@ -14,6 +17,11 @@ const DashNavbar = () => {
         />
         <DynamicBreadcrumb />
       </div>
+      {branch && branch.length > 0 && (
+        <span className="inline-flex items-center px-3 py-1 rounded-full bg-primary-100 text-primary-200 text-xs font-semibold shadow-sm">
+          Branch: {branch?.toLocaleUpperCase()}
+        </span>
+      )}
     </header>
   );
 };
