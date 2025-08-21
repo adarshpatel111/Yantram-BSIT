cd2a3d99d0f53314bb390e17b8f7fd7d60554479
adarshpatelBSIT <150321409+adarshpatelBSIT@users.noreply.github.com>
2025-08-21 17:11:41 +0530

    Update layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,23 +3,25 @@
 import DashNavbar from "@/components/dashboard/dash-navbar";
 import { AppSidebar } from "@/components/dashboard/sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { authClient } from "@/lib/auth-client";
 
 export default function DashboardLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const { data } = authClient.useSession();
+  const role = data?.user?.role ?? "user";
+
   return (
-    <>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset className="flex flex-1 flex-col">
-          <DashNavbar />
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-14 group-has-data-[collapsible=icon]/sidebar-wrapper:pt-12">
-            {children}
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </>
+    <SidebarProvider>
+      <AppSidebar userRole={role} />
+      <SidebarInset className="flex flex-1 flex-col">
+        <DashNavbar />
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-14 group-has-data-[collapsible=icon]/sidebar-wrapper:pt-12">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
