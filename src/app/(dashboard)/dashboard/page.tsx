1a0aae18e3e1270a42953962b3dcee43a5dc91ec
adarshpatelBSIT <150321409+adarshpatelBSIT@users.noreply.github.com>
2025-08-21 17:11:32 +0530

    Update page.tsx

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,7 +4,6 @@ import { authClient } from "@/lib/auth-client";
 import Dashboard from "@/components/dashboard/dashboard/dashboard";
 
 const DashboardPage: React.FC = () => {
-  const signOut = authClient.signOut;
   return (
     <div className="flex flex-col h-full w-full items-center justify-center">
       <Dashboard />
