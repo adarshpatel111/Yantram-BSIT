6046c5e4a8567e4e6f2b278689e2f14446ae6b0e
adarshpatelBSIT <150321409+adarshpatelBSIT@users.noreply.github.com>
2025-08-21 17:11:51 +0530

    Create branches.ts

---
+++ src/components/dashboard/branches.ts
@@ -0,0 +1,6 @@
+export const Branches = [
+  { key: "bharuch", label: "Bharuch" },
+  { key: "vadodara", label: "Vadodara" },
+  { key: "surat", label: "Surat" },
+  { key: "ahmedabad", label: "Ahmedabad" },
+];
