a4c55d30fe1e9d8b758b4a96b26a12657c6b7b75
adarshpatelBSIT <150321409+adarshpatelBSIT@users.noreply.github.com>
2025-08-26 10:40:56 +0530

    Create page.tsx

---
+++ src/app/(lander)/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutInfoVideo from "@/components/lander/about/about-info-video";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <AboutInfoVideo />
+    </>
+  );
+};
+
+export default page;
